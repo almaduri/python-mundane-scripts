43cb7f475a3391374833558b9d9c25e5d6de7511
almaduri <ali.mdrw@gmail.com>
2022-11-03 19:05:45 +0800

    Add starting_episode argument

--- anime.py
+++ anime.py
@@ -6,24 +6,30 @@ def get_num_of_digits(list_of_files: list) -> int:
     return len(str(len(list_of_files)))
 
 
-def get_file_num(num_to_extract: str) -> str:
+def get_file_num(num_to_extract: str, starting_episode: int) -> str:
     file_num = num_to_extract.split()[-1]
 
     if "_" in file_num:
-        file_num = file_num.split("_")[1]
+        file_num = int(file_num.split("_")[1])
+        file_num = starting_episode + file_num - 1
     else:
-        file_num = "1"
+        file_num = starting_episode
     
+    file_num = str(file_num)
+
     return file_num
 
 
-def move_to_dir_according_to_name(anime_name: str, is_move: int) -> None:
+def move_to_dir_according_to_name(anime_name: str, starting_episode: int, is_move: int) -> None:
     parent_path = "D:\\videos\\anime"
 
     os.chdir(os.path.join(parent_path, "raw"))
 
     # Get the total number of digits
-    num_of_digits = get_num_of_digits(os.listdir())
+    if anime_name == "One Piece":
+        num_of_digits = 4
+    else:
+        num_of_digits = get_num_of_digits(os.listdir())
 
     anime_path = os.path.join(parent_path, anime_name)
 
@@ -33,7 +39,7 @@ def move_to_dir_according_to_name(anime_name: str, is_move: int) -> None:
 
     for file in os.listdir():
         num_to_extract, file_ext = os.path.splitext(file)
-        file_num = get_file_num(num_to_extract)
+        file_num = get_file_num(num_to_extract, starting_episode)
 
         file_num = file_num.zfill(num_of_digits)
         new_name = f"{file_num} {anime_name}{file_ext}"
--- automation.py
+++ automation.py
@@ -7,18 +7,20 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-n", "--name", dest="name", help="Anime Name")
 parser.add_argument("-o", "--option", dest="option", help="Option")
+parser.add_argument("-s", "--starting_episode", dest="starting_episode", default=1, help="Starting Episode", type=int)
 parser.add_argument("-m", "--move", dest="move", default=0, help="Is Move", type=int)
 
 args = parser.parse_args()
 
 anime_name = args.name
 option = args.option
+starting_episode = args.starting_episode
 is_move = args.move
 
 if option:
     match option.casefold():
         case "anime":
-            anime.move_to_dir_according_to_name(anime_name, is_move) if anime_name else print("No Name Specified")
+            anime.move_to_dir_according_to_name(anime_name, starting_episode, is_move) if anime_name else print("No Name Specified")
         case "t7":
             t7.move_to_dir_according_to_name(is_move)
         case _:
