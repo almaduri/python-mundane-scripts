b482f9bf3267c248fbb20e47816dd02590e1cf24
almaduri <ali.mdrw@gmail.com>
2023-11-11 13:42:27 +0700

    Delete option

--- main.py
+++ main.py
@@ -5,23 +5,14 @@ from anime import Anime
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-n', '--name', dest='name', help='Anime Name')
-parser.add_argument('-o', '--option', dest='option', help='Option')
 parser.add_argument('-s', '--starting_episode', dest='starting_episode', default=1, help='Starting Episode', type=int)
 parser.add_argument('-m', '--move', dest='move', default=0, help='Is Move', type=int)
 
 args = parser.parse_args()
 
 anime_name = args.name
-option = args.option
 starting_episode = args.starting_episode
 is_move = args.move
 
-if option:
-    match option.casefold():
-        case 'anime':
-            anime = Anime()
-            anime.move_to_dir_according_to_name(anime_name, starting_episode, is_move) if anime_name else print('No Name Specified')
-        case _:
-            print('Not Found')
-else:
-    print('No Option Specified')
+anime = Anime()
+anime.move_to_dir_according_to_name(anime_name, starting_episode, is_move) if anime_name else print('No Name Specified')
